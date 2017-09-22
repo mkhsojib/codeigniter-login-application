f077a119b9b65619517d2e191a4978e6eacda257
mkhsojib <mkhsojib@gmail.com>
2017-09-22 12:22:08 +0600

    creating file structure

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>login application</title>
+    <title>login application</title>
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
 
-      <script src="<?php echo base_url() ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/jquery.js"></script>
 
-    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>   
-    
+    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
 
 
-</head> 
+</head>
 <body>
+
+<div class="container">
+
+    <div class="col-xs-3">
+
+
+
+
+    </div>
+
+    <div class="col-xs-9">
+
+
+        <?php $this->load->view($main_view); ?>
+
+
+    </div>
+
+
+</div>
 
 </body>
 
 
- </html>
+</html>
