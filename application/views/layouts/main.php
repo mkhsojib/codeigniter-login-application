6e6636eaf2ad95f987f3a2cda7c399de92f4a660
Crystal <crystal@Crystals-MacBook-Air.local>
2017-09-22 12:01:43 +0600

    create main template file

---
+++ application/views/layouts/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login application</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+
+      <script src="<?php echo base_url() ?>assets/js/jquery.js"></script>
+
+    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>   
+    
+
+
+</head> 
+<body>
+
+</body>
+
+
+ </html>
